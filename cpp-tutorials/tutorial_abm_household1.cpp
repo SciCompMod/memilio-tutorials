0dea390957816cc681abc660d93c00580c5d38d5
Sascha <51127093+xsaschako@users.noreply.github.com>
2026-02-19 09:42:41 +0100

    update abm tut

--- cpp-tutorials/tutorial_abm_household1.cpp
+++ cpp-tutorials/tutorial_abm_household1.cpp
@@ -52,68 +52,31 @@ int main()
     // There are 10 households for each household group.
     int n_households = 10;
 
-    // // For more than 1 family households we need families. These are parents and children and randoms (which are distributed like the data we have for these households).
-    // auto child = mio::abm::HouseholdMember(num_age_groups); // A child is 50/50% 0-4 or 5-14.
-    // child.set_age_weight(age_group_0_to_4, 1);
-    // child.set_age_weight(age_group_5_to_14, 1);
+    // For more than 1 family households we need families. These are parents and children and randoms (which are distributed like the data we have for these households).
+    auto child = mio::abm::HouseholdMember(num_age_groups); // A child is 50/50% 0-4 or 5-14.
+    child.set_age_weight(age_group_0_to_4, 1);
+    child.set_age_weight(age_group_5_to_14, 1);
 
-    // auto parent = mio::abm::HouseholdMember(num_age_groups); // A parent is 50/50% 15-34 or 35-59.
-    // parent.set_age_weight(age_group_15_to_34, 1);
-    // parent.set_age_weight(age_group_35_to_59, 1);
+    auto parent = mio::abm::HouseholdMember(num_age_groups); // A parent is 50/50% 15-34 or 35-59.
+    parent.set_age_weight(age_group_15_to_34, 1);
+    parent.set_age_weight(age_group_35_to_59, 1);
 
-    // // Two-person household with one parent and one child.
-    // auto twoPersonHousehold_group = mio::abm::HouseholdGroup();
-    // auto twoPersonHousehold_full  = mio::abm::Household();
-    // twoPersonHousehold_full.add_members(child, 1);
-    // twoPersonHousehold_full.add_members(parent, 1);
-    // twoPersonHousehold_group.add_households(twoPersonHousehold_full, n_households);
-    // add_household_group_to_model(model, twoPersonHousehold_group);
+    // Two-person household with one parent and one child.
+    auto twoPersonHousehold_group = mio::abm::HouseholdGroup();
+    auto twoPersonHousehold_full  = mio::abm::Household();
+    twoPersonHousehold_full.add_members(child, 1);
+    twoPersonHousehold_full.add_members(parent, 1);
+    twoPersonHousehold_group.add_households(twoPersonHousehold_full, n_households);
+    add_household_group_to_model(model, twoPersonHousehold_group);
 
-    // // Three-person household with two parent and one child.
-    // auto threePersonHousehold_group = mio::abm::HouseholdGroup();
-    // auto threePersonHousehold_full  = mio::abm::Household();
-    // threePersonHousehold_full.add_members(child, 1);
-    // threePersonHousehold_full.add_members(parent, 2);
-    // threePersonHousehold_group.add_households(threePersonHousehold_full, n_households);
-    // add_household_group_to_model(model, threePersonHousehold_group);
+    // Three-person household with two parent and one child.
+    auto threePersonHousehold_group = mio::abm::HouseholdGroup();
+    auto threePersonHousehold_full  = mio::abm::Household();
+    threePersonHousehold_full.add_members(child, 1);
+    threePersonHousehold_full.add_members(parent, 2);
+    threePersonHousehold_group.add_households(threePersonHousehold_full, n_households);
+    add_household_group_to_model(model, threePersonHousehold_group);
 
-    // Add one social event with 5 maximum contacts.
-    // Maximum contacs limit the number of people that a person can infect while being at this location.
-    auto event = model.add_location(mio::abm::LocationType::SocialEvent);
-    model.get_location(event).get_infection_parameters().set<mio::abm::MaximumContacts>(5);
-    // Add hospital and ICU with 5 maximum contacs.
-    auto hospital = model.add_location(mio::abm::LocationType::Hospital);
-    model.get_location(hospital).get_infection_parameters().set<mio::abm::MaximumContacts>(5);
-    auto icu = model.add_location(mio::abm::LocationType::ICU);
-    model.get_location(icu).get_infection_parameters().set<mio::abm::MaximumContacts>(5);
-    // Add one supermarket, maximum constacts are assumed to be 20.
-    auto shop = model.add_location(mio::abm::LocationType::BasicsShop);
-    model.get_location(shop).get_infection_parameters().set<mio::abm::MaximumContacts>(20);
-    // At every school, the maximum contacts are 20.
-    auto school = model.add_location(mio::abm::LocationType::School);
-    model.get_location(school).get_infection_parameters().set<mio::abm::MaximumContacts>(20);
-    // At every workplace, maximum contacts are 20.
-    auto work = model.add_location(mio::abm::LocationType::Work);
-    model.get_location(work).get_infection_parameters().set<mio::abm::MaximumContacts>(20);
-
-    // Increase aerosol transmission for all locations
-    model.parameters.get<mio::abm::AerosolTransmissionRates>() = 10.0;
-    // Increase contact rate for all people between 15 and 34 (i.e. people meet more often in the same location)
-    model.get_location(work)
-        .get_infection_parameters()
-        .get<mio::abm::ContactRates>()[{age_group_15_to_34, age_group_15_to_34}] = 10.0;
-
-    // People can get tested at work (and do this with 0.5 probability) from time point 0 to day 10.
-    auto validity_period       = mio::abm::days(1);
-    auto probability           = 0.5;
-    auto start_date            = mio::abm::TimePoint(0);
-    auto end_date              = mio::abm::TimePoint(0) + mio::abm::days(10);
-    auto test_type             = mio::abm::TestType::Antigen;
-    auto test_parameters       = model.parameters.get<mio::abm::TestData>()[test_type];
-    auto testing_criteria_work = mio::abm::TestingCriteria();
-    auto testing_scheme_work   = mio::abm::TestingScheme(testing_criteria_work, validity_period, start_date, end_date,
-                                                         test_parameters, probability);
-    model.get_testing_strategy().add_scheme(mio::abm::LocationType::Work, testing_scheme_work);
 
     // Assign infection state to each person.
     // The infection states are chosen randomly with the following distribution
@@ -146,9 +109,6 @@ int main()
         }
     }
 
-    // During the lockdown, social events are closed for 90% of people.
-    auto t_lockdown = mio::abm::TimePoint(0) + mio::abm::days(10);
-    mio::abm::close_social_events(t_lockdown, 0.9, model.parameters);
 
     // Set start and end time for the simulation.
     auto t0   = mio::abm::TimePoint(0);
